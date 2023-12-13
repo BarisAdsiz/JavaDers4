0fa37809380c4c3375c037a24e1aa7494f609ac1
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:19:28 +0300

    Add files via upload

---
+++ src/ders_4/Ders_4.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_4;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // byte -128 ve 127 arası sayı tutar
+        byte kucukSayi = 120;
+        System.out.println(kucukSayi);
+
+        short sayi = 32000;
+        System.out.println(sayi);
+        
+        int genelSayi = 2131212;
+        System.out.println(genelSayi);
+        
+        long buyukSayi = 425;
+        System.out.println(buyukSayi);
+        
+        double oran = 3.14;
+        System.out.println(oran);
+        
+        char karakter = '$';
+        System.out.println(karakter);
+    }
+    
+}
